264c6c42eac0645c03c2c400ba65ec12af5e5320
Vichukano <vichukano@gmail.com>
2018-08-15 22:03:47 +0300

    5.1.4. Создать convert(Iterator<Iterator>) [#152]

--- junior/chapter_001/src/main/java/ru/job4j/collections/iterator/Main.java
+++ junior/chapter_001/src/main/java/ru/job4j/collections/iterator/Main.java
@@ -1,20 +1,28 @@
 package ru.job4j.collections.iterator;
 
-import ru.job4j.collections.iterator.even.EvenIterator;
+
+import ru.job4j.collections.iterator.convert.IteratorOfIterators;
+
+import java.util.Arrays;
+import java.util.Iterator;
 
 /**
  * Main class.
  */
 public class Main {
+    public static IteratorOfIterators<Integer> it;
+
     public static void main(String[] args) {
-        int[] nums = {2, 4, 5, 1, 10, 1, 6, 6, 8};
 
-        EvenIterator evenIterator = new EvenIterator(nums);
+        Iterator<Integer> it1 = Arrays.asList(1, 2, 3, 10).iterator();
+        Iterator<Integer> it2 = Arrays.asList(4, 5, 89, 6).iterator();
+        Iterator<Integer> it3 = Arrays.asList(7, 8, 9, 0).iterator();
+        Iterator<Iterator<Integer>> its = Arrays.asList(it1, it2, it3).iterator();
+        IteratorOfIterators iteratorOfIterators = new IteratorOfIterators(it);
+        it = iteratorOfIterators.convert(its);
 
-        System.out.println("Count of even numbers = " + evenIterator.getSize());
-
-        while (evenIterator.hasNext()) {
-            System.out.println(evenIterator.next());
+        while (it.hasNext()) {
+            System.out.println(it.next());
         }
     }
 }
---
+++ junior/chapter_001/src/main/java/ru/job4j/collections/iterator/convert/IteratorOfIterators.java
@@ -0,0 +1,68 @@
+package ru.job4j.collections.iterator.convert;
+
+import java.util.Iterator;
+import java.util.*;
+
+/**
+ * Класс итератор итераторов.
+ *
+ * @param <Integer>
+ */
+public class IteratorOfIterators<Integer> implements Iterator {
+    private Iterator<Iterator<Integer>> iterator;
+    private Iterator<Integer> currentIterator = null;
+
+    /**
+     * Стандартный конструктор.
+     *
+     * @param iterator - Iterator<Iterator<Integer>>.
+     */
+    public IteratorOfIterators(Iterator<Iterator<Integer>> iterator) {
+        this.iterator = iterator;
+    }
+
+    /**
+     * Метод, реализующий итератор для Iterator<Iterator>.
+     *
+     * @param it - Итератор итератора целых чисел.
+     * @return Итератор.
+     */
+    public IteratorOfIterators<Integer> convert(Iterator<Iterator<Integer>> it) {
+        return new IteratorOfIterators<>(it);
+    }
+
+    @Override
+    public boolean hasNext() {
+        selectCurrentIterator();
+        return (currentIterator != null && currentIterator.hasNext());
+    }
+
+    @Override
+    public Integer next() {
+        selectCurrentIterator();
+        if (currentIterator == null) {
+            throw new NoSuchElementException();
+        }
+        return currentIterator.next();
+    }
+
+    /**
+     * Метод, выбирает текущий итератор.
+     * Если текущий итератор существует и имеет следующие значения, то пропускаем и проверяем следующий итератор.
+     * Иначе переходим к следующему итератору.
+     */
+    private void selectCurrentIterator() {
+        if (currentIterator != null && currentIterator.hasNext()) {
+            return; // Пустой return в методе void пропускает шаг.
+        } else {
+            currentIterator = null;
+            while (iterator.hasNext()) {
+                Iterator<Integer> nextIterator = iterator.next();
+                if (nextIterator.hasNext()) {
+                    currentIterator = nextIterator;
+                    break;
+                }
+            }
+        }
+    }
+}
