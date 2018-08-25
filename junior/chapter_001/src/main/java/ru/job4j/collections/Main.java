64d92504657ac8b87681feebbc18c65c7def6c88
Vichukano <vichukano@gmail.com>
2018-08-26 00:29:19 +0300

    2. Не перекрывать equals hashCode [#1005][#68204]

--- junior/chapter_001/src/main/java/ru/job4j/collections/Main.java
+++ junior/chapter_001/src/main/java/ru/job4j/collections/Main.java
@@ -1,6 +1,11 @@
 package ru.job4j.collections;
 
-import ru.job4j.collections.list.Node;
+
+import ru.job4j.collections.map.User;
+
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Main class.
@@ -9,7 +14,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //TODO
+        User user1 = new User("One", 2, new GregorianCalendar(1991, 2, 13));
+        User user2 = new User("One", 2, new GregorianCalendar(1991, 2, 13));
 
+        /**
+         * Методы equals и hashCode не переопределены.
+         */
+        Map<User, Object> map = new HashMap<>();
+        map.put(user1, "first");
+        map.put(user2, "second");
+        System.out.println(map);
     }
 }
