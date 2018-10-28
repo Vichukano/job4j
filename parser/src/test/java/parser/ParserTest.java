9f454347cb00e4924b6c14a3e14bc4e5744705ad
Vichukano <vichukano@gmail.com>
2018-10-28 17:26:12 +0300

    Парсер вакансий на sql.ru  - настройка бд для Travis CI 4.3. XML XSLT JDBC Оптимизация  - рефакторинг.

--- parser/src/test/java/parser/ParserTest.java
+++ parser/src/test/java/parser/ParserTest.java
@@ -14,11 +14,14 @@ public class ParserTest {
     public void whenAddStringItShouldConvertToDate() {
         Parser parser = new Parser();
         Date date = parser.formatStringToDate("19 окт 18, 14:32");
-        assertThat(date.toString(), is("Fri Oct 19 14:32:00 MSK 2018"));
+        String dateToString = date.toString().replace("MSK 2018", "");
+        assertThat(dateToString, is("Fri Oct 19 14:32:00 "));
         date = parser.formatStringToDate("9 сен 17, 15:31");
-        assertThat(date.toString(), is("Sat Sep 09 15:31:00 MSK 2017"));
+        dateToString = date.toString().replace("MSK 2017", "");
+        assertThat(dateToString, is("Sat Sep 09 15:31:00 "));
         date = parser.formatStringToDate("28 май 18, 15:31");
-        assertThat(date.toString(), is("Mon May 28 15:31:00 MSK 2018"));
+        dateToString = date.toString().replace("MSK 2018", "");
+        assertThat(dateToString, is("Mon May 28 15:31:00 "));
     }
 
     @Test
