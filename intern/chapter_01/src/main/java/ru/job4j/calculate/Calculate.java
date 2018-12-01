22896734d989c4d9eed56a4bfb5eca2d35a3a8e0
Vichukano <vichukano@gmail.com>
2018-12-01 21:44:12 +0300

    Произвести автоматическое тестирование всех сервлетов. - небольшой рефакторинг.

--- intern/chapter_01/src/main/java/ru/job4j/calculate/Calculate.java
+++ intern/chapter_01/src/main/java/ru/job4j/calculate/Calculate.java
@@ -12,7 +12,7 @@ public class Calculate {
 	*@param args - args.
 	*/
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.print("Hello World!");
 	}
 	
 	/**
--- intern/chapter_01/src/test/java/ru/job4j/calculate/CalculateTest.java
+++ intern/chapter_01/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -29,6 +29,6 @@ public class CalculateTest {
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         System.setOut(new PrintStream(out));
         Calculate.main(null);
-        assertThat(out.toString(), is("Hello World!\r\n"));
+        assertThat(out.toString(), is("Hello World!"));
     }
 }
