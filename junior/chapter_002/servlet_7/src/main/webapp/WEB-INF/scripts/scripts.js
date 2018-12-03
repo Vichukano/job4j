1549a4a6b2b158496146474d8d609090a65c6048
Vichukano <vichukano@gmail.com>
2018-12-03 23:54:00 +0300

    Заполнить динамически таблицу.

--- junior/chapter_002/servlet_7/src/main/webapp/WEB-INF/scripts/scripts.js
+++ junior/chapter_002/servlet_7/src/main/webapp/WEB-INF/scripts/scripts.js
@@ -1,3 +1,6 @@
+/*
+Function for validating input in form.
+ */
 function validate() {
     var name = document.getElementById("name").value;
     var surname = document.getElementById("surname").value;
@@ -8,6 +11,7 @@ function validate() {
         surnameContainer.classList.remove("has-error");
         nameContainer.classList.add("has-success");
         surnameContainer.classList.add("has-success");
+        addRow();
     } else {
         nameContainer.classList.remove("has-success");
         surnameContainer.classList.remove("has-success");
@@ -15,4 +19,27 @@ function validate() {
         surnameContainer.classList.add("has-error");
         alert("Заполните поля!");
     }
+}
+/*
+Function for adding row to table.
+ */
+function addRow() {
+    var name = document.getElementById("name").value;
+    var surname = document.getElementById("surname").value;
+    var sex;
+    var description = document.getElementById("description").value;
+    if (document.getElementById("maleRadio").checked) {
+        sex = document.getElementById("maleRadio").value;
+    }
+    if (document.getElementById("femRadio").checked) {
+        sex = document.getElementById("femRadio").value;
+    }
+    var tbody = document.getElementById("table1").getElementsByTagName("tbody")[0];
+    var row = document.createElement("tr");
+    var query = "<td>" + name +"</td>"
+        + "<td>" + surname +"</td>"
+        + "<td>" + sex +"</td>"
+        + "<td>" + description + "</td>";
+    row.innerHTML = query;
+    tbody.appendChild(row);
 }
