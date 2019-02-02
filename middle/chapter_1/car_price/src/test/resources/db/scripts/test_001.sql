c82afc67017709315e51b7fc0768f641493a120c
Vichukano <vichukano@gmail.com>
2019-02-02 21:03:28 +0300

    Интеграционное тестирование.

--- middle/chapter_1/car_price/src/test/resources/db/scripts/test_001.sql
+++ middle/chapter_1/car_price/src/test/resources/db/scripts/test_001.sql
@@ -1,24 +1,21 @@
 CREATE TABLE car_body (
   body_id BIGSERIAL PRIMARY KEY,
-  type    VARCHAR(255)
+  type VARCHAR(255)
 );
 
-INSERT INTO car_body (type)
-VALUES ('sedan'), ('hatchback'), ('crossover');
+INSERT INTO car_body (type) VALUES ('sedan'), ('hatchback'), ('crossover');
 
 CREATE TABLE car_engine (
   engine_id BIGSERIAL PRIMARY KEY,
-  type      VARCHAR(255)
+  type VARCHAR(255)
 );
 
-INSERT INTO car_engine (type)
-VALUES ('petrol'), ('diesel'), ('hybrid');
+INSERT INTO car_engine (type) VALUES ('petrol'), ('diesel'), ('hybrid');
 
 CREATE TABLE car_transmission (
   transmission_id BIGSERIAL PRIMARY KEY,
-  type            VARCHAR(255)
+  type VARCHAR(255)
 );
 
-INSERT INTO car_transmission (type)
-VALUES ('mechanical'), ('automatic'), ('robot');
+INSERT INTO car_transmission (type) VALUES ('mechanical'), ('automatic'), ('robot');
 
