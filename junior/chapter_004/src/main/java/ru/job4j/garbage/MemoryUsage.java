93972f1a70fb9c4a1d212a5187d6aaf08e8b0c8e
Vichukano <vichukano@gmail.com>
2019-02-04 03:22:44 +0300

    Интеграционное тестирование. -добавлен скрипт наполнения базы для тестов.

---
+++ junior/chapter_004/src/main/java/ru/job4j/garbage/MemoryUsage.java
@@ -0,0 +1,65 @@
+package ru.job4j.garbage;
+
+import jdk.nashorn.internal.ir.debug.ObjectSizeCalculator;
+
+public class MemoryUsage {
+
+    public static void main(String[] args) {
+        System.out.println("Вес объекта user = " + ObjectSizeCalculator.getObjectSize(new User()));
+        for (int i = 0; i != 12_000; i++) {
+            new User("Test", i);
+        }
+    }
+
+    public static void info() {
+        Runtime runtime = Runtime.getRuntime();
+        System.out.println("---Heap memory statistics---");
+        System.out.println("Max memory: " + runtime.maxMemory());
+        System.out.println("Available memory: " + runtime.totalMemory());
+        System.out.println("Free memory: " + runtime.freeMemory());
+        System.out.println("Used memory: " + (runtime.totalMemory() - runtime.freeMemory()));
+
+    }
+
+    private static class User {
+        private String name;
+        private int age;
+
+        public User() {
+
+        }
+
+        public User(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("User name = %s, age = %d", name, age);
+        }
+
+        @Override
+        protected void finalize() throws Throwable {
+            super.finalize();
+            System.out.println("Finalize user object.");
+            info();
+        }
+    }
+}
